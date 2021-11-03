d049daec0a36ab4316631788289dc5cc7821748a
waythin <wathinmarma@gmail.com>
2021-11-03 22:16:00 +0600

    seeker added

--- Job_For_All/app/Http/Kernel.php
+++ Job_For_All/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'AdminIsValidCheck' => \App\Http\Middleware\AdminIsValidCheck::class,
+        'SeekerAuth' => \App\Http\Middleware\SeekerAuth::class,
     ];
 }
